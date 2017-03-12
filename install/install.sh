e81df5cd0678d36f7f6359870504e4faa512fb46
draducanu <raducanu_dragos12@yahoo.com>
2017-03-12 23:45:26 +0200

    commit

--- install/install.sh
+++ install/install.sh
@@ -192,9 +192,12 @@ setup() {
     echo 'Installing base system'
     install_base
 
+    echo 'Setting fstab'
+    set_fstab
+
     echo 'Chrooting into installed system to continue setup...'
     cp $0 /mnt/setup.sh
-    arch-chroot /mnt ./setup.sh chroot
+    arch-chroot /mnt /bin/bash ./setup.sh chroot
 
     if [ -f /mnt/setup.sh ]
     then
@@ -210,9 +213,6 @@ setup() {
 configure() {
 
     echo '-------------Configuration---------------'
-
-    echo 'Setting fstab'
-    set_fstab
 
 #    echo 'Setting chroot'
 #    set_chroot
