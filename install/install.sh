ad29ff3a954971d08b5db8237594c9a3f0dd92a0
draducanu <raducanu_dragos12@yahoo.com>
2017-03-18 23:33:18 +0200

    commit

--- install/install.sh
+++ install/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 
-. readProps.sh
+installDir=/archInstall
 
 rm -r $installDir
 mkdir $installDir
 
+curl -L https://raw.githubusercontent.com/Inonut/Arch/master/install/constants.properties > $installDir/constants.properties
+curl -L https://raw.githubusercontent.com/Inonut/Arch/master/install/readProps.sh > $installDir/readProps.sh
 curl -L https://raw.githubusercontent.com/Inonut/Arch/master/install/setup.sh > $installDir/setup.sh
 curl -L https://raw.githubusercontent.com/Inonut/Arch/master/install/config-root.sh > $installDir/config-root.sh
 curl -L https://raw.githubusercontent.com/Inonut/Arch/master/install/config-user.sh > $installDir/config-user.sh
 
+echo "installDir=/archInstall" >> $installDir/constants.properties
 
 sh $installDir/setup.sh
