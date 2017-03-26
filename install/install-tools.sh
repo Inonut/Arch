f017ed9ce90e425a38c9096ba6d75e1b305087a5
dragos <raducanudragos12@gmail.com>
2017-03-26 16:35:33 +0300

    commit

--- install/install-tools.sh
+++ install/install-tools.sh
@@ -4,7 +4,14 @@ installDir=/archInstall
 
 . $installDir/pacaut-install.sh
 
-pacman -S maven gradle git notepadqq virtualbox
-pacaur -S jdk intellij-idea-ultimate-edition
+#virtual box
+sudo pacman -S virtualbox virtualbox-host-modules-arch
+sudo modprobe -a vboxdrv
+sudo echo 'vboxdrv' >> /etc/modules-load.d/virtualbox.conf
+sudo gpasswd -a $USER vboxusers
 
-archlinux-java java-8-jdk
+
+sudo pacman -S maven gradle git notepadqq
+sudo pacaur -S jdk intellij-idea-ultimate-edition
+
+sudo archlinux-java java-8-jdk
